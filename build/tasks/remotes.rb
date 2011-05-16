8c57554375a072683ba5ea82daff815c6d9cd504
Jean-Paul S. Boodhoo <jp@developwithpassion.com>
2011-05-16 09:00:55 -0600

    Added the remotes script

--- build/tasks/remotes.rb
+++ build/tasks/remotes.rb
@@ -2,5 +2,5 @@ desc "Configure the remote connection to JP"
 
 task :setup_remote_to_jp do
   `git remote rm jp`
-  `git remote add jp git://github.com/developwithpassion/nbdndp.git`
+  `git remote add jp git://github.com/developwithpassion/dwpdp.git`
 end
